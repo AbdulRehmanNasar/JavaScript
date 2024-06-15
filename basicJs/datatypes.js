2df9be0c14d656366dc9e2d43b56d15126fba472
AbdulRehmanNasar <163045826+AbdulRehmanNasar@users.noreply.github.com>
2024-06-15 08:28:29 +0000

    DataTypes and documentation

---
+++ basicJs/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treats all JS code as newest version
+// alert(3+3) we are using NodeJs not browser
+
+// for documentation tc39.es and MDN
+
+console.log(3+3); console.log("Hello"); //use good coding practices
+
+let name = "Reno"
+let age = 18
+let city  //variable without any value have Undefined type
+let isLoggedIn = true
+let arr = null  //null is of Object type
+
+console.log(typeof name);
+console.log([typeof name, typeof age, typeof city, typeof isLoggedIn]);
+console.log(typeof arr);
