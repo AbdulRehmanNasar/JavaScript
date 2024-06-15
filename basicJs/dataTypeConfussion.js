ef99fd7c7878e36d2a9e3c65eaa34fc55e4ccf0d
AbdulRehmanNasar <163045826+AbdulRehmanNasar@users.noreply.github.com>
2024-06-15 20:23:53 +0000

    Some confussions in DataTypes

---
+++ basicJs/dataTypeConfussion.js
@@ -0,0 +1,45 @@
+let score = 33
+let score1 = "331bc"
+let score2 = null
+let score3
+let score4 = true
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+
+
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+
+
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(typeof valueInNumber4);
+
+
+
+console.log(valueInNumber);
+console.log(valueInNumber1);
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+console.log(valueInNumber4);
+
+// 33 -> number type-> number
+// 33abc -> number type-> number
+// null -> 0 type-> number
+// undefined -> NaN type-> number
+// boolean -> 1 (true) 0 (false) type-> number
+
+// More conversions into:
+// Boolean
+// String
