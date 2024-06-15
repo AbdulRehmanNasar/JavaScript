908865052fc3aae081c54e80cf6bea939dad71d7
AbdulRehmanNasar <163045826+AbdulRehmanNasar@users.noreply.github.com>
2024-06-15 22:38:13 +0000

    Comparison bw different DataTypes

---
+++ basicJs/comparisons.js
@@ -0,0 +1,28 @@
+// console.log("2"<2);
+// console.log("2">2);
+// console.log("2"<=2);
+// console.log("2">=2);
+
+// console.log(2<"2");
+// console.log(2>"2");
+// console.log(2<="2");
+// console.log(2>="2");
+// console.log(2=="2");
+
+
+// before comparison null is converted into Number by default
+// console.log(null<0);
+// console.log(null>0);
+// console.log(null<=0);
+// console.log(null>=0);
+// console.log(null==0);
+
+// console.log(undefined<2);
+// console.log(undefined>0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+
+// Strict Comparison (===)
+
+console.log("2"===2);
